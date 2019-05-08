1e133f4a85fc734f9e9bdaf09a32c45203201bf7
Bartłomiej Olech <olech.bartlomiej@gmail.com>
2019-05-08 23:58:25 +0200

    first initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './components/login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    isLoggedIn: true
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          {this.state.isLoggedIn ? (
+            <div>
+              <Route exact path='/' component={Login}></Route>
+            </div>
+          ) : null}
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
